64a68696c619b40032e547394e08a987d545963f
chemingjun <chemingjun@weidian.com>
2016-11-07 18:31:28 +0800

    init all

--- example/sample.js
+++ example/sample.js
@@ -2,12 +2,20 @@
  * sample型单元测试
  * Author: CheMingjun
  */
+var assert = require('assert');
 
-//@test.step
-var start = function () {
+'@test.step';
+var t0 = function () {
     var ts = 'aaaaa';
     for (var i = 0; i < 10000; i++) {
         ts += 'aa';
     }
     return ts;
+}
+
+'@test.step';
+var t1 = function () {
+    var t = 900;
+    t = 900/9;
+    assert.equal(t,100);
 }
